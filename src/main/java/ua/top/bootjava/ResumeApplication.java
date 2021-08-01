9cebd87841d93499f508f75bfa4cc3263b8910d6
taras <promverstat@ukr.net>
2021-08-01 20:09:55 +0300

    2.2 Refactor model. Spring Data JPA @Query. Spring data Rest

--- src/main/java/ua/top/bootjava/ResumeApplication.java
+++ src/main/java/ua/top/bootjava/ResumeApplication.java
@@ -18,6 +18,6 @@ public class ResumeApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(userRepository.findAll());
+        System.out.println(userRepository.findByLastNameContainingIgnoreCase("last"));
     }
 }
